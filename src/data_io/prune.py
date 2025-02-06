e8fba0dabe9be56fb6c2e6690d976d9813a5e002
Akhilesh Yeluru <ayeluru@ucsd.edu>
2025-02-06 15:13:45 -0800

    prune_unzipped_folders now deletes all the necessary folders in paralell and updates tqdm to be displayed at the command line. currently deleting files. no further commit means it works

--- src/data_io/prune.py
+++ src/data_io/prune.py
@@ -1,6 +1,19 @@
 import os
 import zipfile
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+import multiprocessing
+
+def delete_folder(folder_path, progress_bar):
+    for root, dirs, files in os.walk(folder_path, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+            progress_bar.update(1)
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+            progress_bar.update(1)
+    os.rmdir(folder_path)
+    progress_bar.update(1)
 
 def prune_unzipped_folders(zip_folder, unzipped_folder):
     # Get the list of zip files (without extension)
@@ -11,16 +24,26 @@ def prune_unzipped_folders(zip_folder, unzipped_folder):
     
     # Find unzipped folders that do not have a corresponding zip file
     folders_to_delete = unzipped_folders - zip_files
+
+    # Print the folders that are going to be deleted
+    print("The following folders will be deleted:")
+    for folder in folders_to_delete:
+        print(folder)
+    
+    # Calculate the total number of files and directories to delete
+    total_items_to_delete = sum(len(files) + len(dirs) + 1 for folder in folders_to_delete for _, dirs, files in os.walk(os.path.join(unzipped_folder, folder)))
     
     # Delete the unzipped folders that do not have a corresponding zip file
-    for folder in tqdm(folders_to_delete, desc="Deleting folders", unit="folder"):
-        folder_path = os.path.join(unzipped_folder, folder)
-        for root, dirs, files in os.walk(folder_path, topdown=False):
-            for name in files:
-                os.remove(os.path.join(root, name))
-            for name in dirs:
-                os.rmdir(os.path.join(root, name))
-        os.rmdir(folder_path)
+    with tqdm(total=total_items_to_delete, desc="Deleting items", unit="item") as progress_bar:
+        with ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
+            futures = []
+            for folder in folders_to_delete:
+                folder_path = os.path.join(unzipped_folder, folder)
+                folder_path = os.path.join(unzipped_folder, folder)
+                progress_bar.set_description(f"Deleting folder: {folder}")
+                futures.append(executor.submit(delete_folder, folder_path, progress_bar))
+            for future in futures:
+                future.result()
 
 # Example usage
 if __name__ == "__main__":
