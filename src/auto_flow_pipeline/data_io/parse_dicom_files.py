5f20229020a612751bb8d5cf7b07f39f35f3a50b
Akhilesh Yeluru <ayeluru@ucsd.edu>
2025-02-13 15:10:27 -0800

    moved logging function to its own file and updated parse_dicom_files to import from that file

--- src/auto_flow_pipeline/data_io/parse_dicom_files.py
+++ src/auto_flow_pipeline/data_io/parse_dicom_files.py
@@ -5,19 +5,8 @@ import pydicom
 from tqdm import tqdm
 from multiprocessing import Pool, cpu_count
 import numpy as np
+from auto_flow_pipeline.data_io.logging import log_message
 
-# Custom logging function
-def log_message(message, log_file):
-    """
-    Logs a message to both the console and a log file.
-    
-    Parameters:
-    message (str): The message to log.
-    log_file (str): The path to the log file.
-    """
-    # tqdm.write(message)
-    with open(log_file, 'a') as f:
-        f.write(message + '\n')
 
 def is_dicom_file(file_path, log_file):
     """
