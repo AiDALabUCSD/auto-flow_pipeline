3d28a1d3b1b0fe1abac40a591c5628381a932f93
Akhilesh Yeluru <ayeluru@ucsd.edu>
2025-03-03 18:01:54 -0800

    added a comment with the path to a different segnet model for debugging

--- src/auto_flow_pipeline/inference/segnet/config.py
+++ src/auto_flow_pipeline/inference/segnet/config.py
@@ -9,6 +9,8 @@ MODEL_FOLDER = "/home/ayeluru/mnt/maxwell/projects/Aorta_pulmonary_artery_locali
 MODEL_NAME = "segnet.hdf5"
 MODEL_PATH = os.path.join(MODEL_FOLDER, MODEL_NAME)
 
+# MODEL_PATH = "/home/ayeluru/4d-flow-automation/segnet/training_outputs/augmented_dyn-rng_trns_rot_through_def_tr-3e-5/b32_e1000_augmented_dyn-rng_trns_rot_through_def_tr-3e-5_chkpt--1000-0.0926-continue-2.hdf5"
+
 ###############################################################################
 # Other
 ###############################################################################
